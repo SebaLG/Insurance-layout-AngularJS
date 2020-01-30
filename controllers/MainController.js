7abe342a25bcea6a0cabef419f40fdd356e4772a
Sebastian Lopez Garcia <sebastian.lopez@admiral-eu.com>
2020-01-30 11:23:51 +0100

    Coverages

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -4,36 +4,63 @@ app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
         .when('/home', {
             templateUrl: 'components/home.html',
-            controller: 'HomeController',
-            controllerAs: 'Home'
+            controller: 'MainController',
+            controllerAs: 'controller'
         })
         .when('/coverages', {
             templateUrl: 'components/coverages.html',
-            controller: 'CoveragesController',
-            controllerAs: 'Coverages'
+            controller: 'MainController',
+            controllerAs: 'controller'
         })
         .when('/personalForm', {
             templateUrl: 'components/personalForm.html',
-            controller: 'FormController',
-            controllerAs: 'Form'
+            controller: 'MainController',
+            controllerAs: 'controller'
         })
         .when('/summary', {
             templateUrl: 'components/summary.html',
-            controller: 'SummaryController',
-            controllerAs: 'Summary'
+            controller: 'MainController',
+            controllerAs: 'controller'
         })
         .otherwise({
-            redirectTo: '/'
+            redirectTo: '/home'
         });
 }]);
 
 app.controller('MainController', ['$route', '$routeParams', '$location', function($route, $routeParams, $location) {
-    // this.hdetails = {
-    //     name: 'temp'
-    // };
+
     this.$route = $route;
     this.$routeParams = $routeParams;
     this.$location = $location;
+
+    var controller = this;
+
+    controller.coverages = [
+        { name: "RCA", price: "100€", selected: "false" },
+        { name: "Kasko", price: "150€", selected: "false" },
+        { name: "Collision", price: "10€", selected: "false" },
+        { name: "Fire", price: "99€", selected: "false" }
+    ];
+
+    console.log("TCL: controller.coverages", controller.coverages)
+
+    // method to add an item to the list
+    controller.addCoverages = () => {
+
+
+        console.log("TCL: controller.coverages", controller.coverages)
+            // Put the object into storage
+        localStorage.setItem('coverages', JSON.stringify(controller.coverages));
+        $('#form-coverages').submit();
+        $location.path("/personalForm");
+
+
+
+    };
+
+
+
+
 }]);
 
 app.controller("HomeController", function($scope) {
@@ -51,5 +78,3 @@ app.controller("FormController", function($scope) {
 app.controller("SummaryController", function($scope) {
     $scope.message = "SUMMARY PAGE";
 });
-
-;
